ef8f9223daf82cd75579a34ed7c880d3ed08096c
Dominic Holmes <dominic.holmes@alliantist.com>
2017-10-04 13:05:18 +0200

    Make RDL kick in only if the RDL environment variable is set
    Given how slow it is for development purposes (test and app load times)
    it makes sense to load this on a case by case basis. We will also want
    to enable for the CI run.

--- lib/rdl/boot_rails.rb
+++ lib/rdl/boot_rails.rb
@@ -1,15 +1,13 @@
-if Rails.env.development? || Rails.env.test?
+if ENV['RDL'] && !Rails.env.production?
   require 'rdl/boot'
   require 'types/core'
 
   # Don't load the Rails types as these don't seem to work for Rails 3.
   # require_relative "../types/rails/_helpers.rb" # load type aliases first
   # Dir[File.dirname(__FILE__) + "/../types/rails/**/*.rb"].each { |f| require f }
-elsif Rails.env.production?
+else
   require 'rdl_disable'
   class ActionController::Base
     def self.params_type(typs); end
   end
-else
-  raise RuntimeError, "Don't know what to do in Rails environment #{Rails.env}"
 end
